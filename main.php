4da2d662268516690cb12ba7d81f9a70320da5bb
= <=>
2024-01-29 14:41:17 +0530

    first commit

---
+++ main.php
@@ -0,0 +1,609 @@
+<!doctype html>
+
+<html class="no-js" >
+
+    <head>
+
+    	<!-- metas -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       	<meta name="description" content="Daniels is a responsive creative template">
+		<meta name="keywords" content="portfolio, personal, corporate, business, parallax, creative, agency">
+
+		<!-- title -->
+		<title>Lalit</title>
+
+		<!-- favicon -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="img/favicon.ico" rel="icon" type="image/png">
+
+        <!-- bootstrap css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<!-- google fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800" rel="stylesheet">
+
+		<!-- owl carousel CSS -->
+		<link rel="stylesheet" href="css/owl.carousel.min.css">
+		<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+		<!-- magnific-popup CSS -->
+		<link rel="stylesheet" href="css/magnific-popup.css">
+
+		<!-- animate.min CSS -->
+		<link rel="stylesheet" href="css/animate.min.css">
+
+		<!-- Font Icon Core CSS -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/et-line.css">
+
+		<!-- Core Style Css -->
+        <link rel="stylesheet" href="css/style.css">
+
+        <!--[if lt IE 9]-->
+		<script src="js/html5shiv.min.js"></script>
+		<!--[endif]-->
+
+		<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+/>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+
+<style>
+
+	
+	.swiper {
+      width: 100%;
+      height: 100px;
+    }
+
+    .swiper-slide {
+      text-align: center;
+      font-size: 18px;
+      background: #fff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .swiper-slide img {
+      display: block;
+      width: 90px;
+      height: 90px;
+      object-fit: cover;
+    }
+
+	.swiper-wrapper{
+		height:200px;
+	}
+</style>
+
+    </head>
+    
+    <body>
+
+    	<!-- ====== Preloader ======  -->
+	    <div class="loading">
+			<div class="load-circle">
+			</div>
+		</div>
+		<!-- ======End Preloader ======  -->
+
+		<!-- ====== Navgition ======  -->
+		<nav class="navbar navbar-default">
+		  <div class="container">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-icon-collapse" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+
+		       <!-- logo -->
+		      <a class="logo" href="#">Lalit</a>
+
+		    </div>
+
+		    <!-- Collect the nav links, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="nav-icon-collapse">
+		      
+			  <!-- links -->
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#" data-scroll-nav="0" class="active">Home</a></li>
+		        <li><a href="#" data-scroll-nav="1">About</a></li>
+				<li><a href="#" data-scroll-nav="2">Skills</a></li>
+		        <li><a href="#" data-scroll-nav="3">Works</a></li>
+		        <li><a href="#" data-scroll-nav="6">Contact</a></li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container -->
+		</nav>
+		<!-- ====== End Navgition ======  -->
+
+		<!-- ====== Header ======  -->
+		<section id="home" class="header" data-scroll-index="0" style="background-image: url(https://images.unsplash.com/photo-1614741118887-7a4ee193a5fa?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);" data-stellar-background-ratio="0.8">
+
+			<div class="v-middle">
+				<div class="container">
+					<div class="row">
+
+						<!-- caption -->
+						<div class="caption">
+							<h5>Hello</h5>
+							<h1 class="cd-headline clip">
+					            <span class="blc">I Am </span>
+					            <span class="cd-words-wrapper">
+					              <b class="is-visible">Lalit Harwate</b>
+					              <b>Backend Developer</b>
+					            </span>
+			          		</h1>
+
+			          		<!-- social icons -->
+			          		<div class="social-icon">
+			          			<a href="#0">
+			          				<span><i class="fa fa-github" aria-hidden="true"></i></span>
+			          			</a>
+			          		
+			          			<a href="#0">
+			          				<span><i class="fa fa-linkedin" aria-hidden="true"></i></span>
+			          			</a>
+			          			
+			          			
+			          		</div>
+						</div>
+						<!-- end caption -->
+					</div>
+				</div><!-- /row -->
+			</div><!-- /container -->
+		</section>
+		<!-- ====== End Header ======  -->
+
+		<!-- ====== Hero ======  -->
+		<section class="hero section-padding pb-70" data-scroll-index="1">
+			<div class="container">
+				<div class="row">
+
+					<!-- hero image -->
+					<div class="col-md-5">
+						<div class="hero-img mb-30">
+							<img src="img/profile.jpg" alt="">
+						</div>
+					</div>
+
+					<!-- content -->
+					<div class="col-md-7">
+						<div class="content mb-30">
+							<h3>About Me.</h3>
+							<span class="sub-title">Backend Web Developer</span>
+							<p>I am a backend web developer with proficiency in several programming languages and technologies. My primary expertise lies in Laravel, PHP, MySQL, and REST API development. I am well-versed in building robust and scalable server-side solutions to support various web applications.
+								<br>
+								I have a solid understanding of web development fundamentals, including HTML, CSS, and JavaScript. Additionally, I have experience with the React library for building dynamic and interactive user interfaces.
+								<br>
+								My skill set includes:
+								<br>
+									1. Laravel: Developing backend logic, RESTful APIs, and integrating databases.
+									<br>
+									2. PHP: Writing server-side scripts and implementing backend functionality.
+									<br>	
+									3. MySQL: Designing and optimizing relational databases.
+								<br>
+									4. REST API: Creating and consuming RESTful APIs for seamless communication between different components.
+									<br>
+
+									5. AJAX: Enhancing user experiences by making asynchronous requests to the server.
+									<br>
+									6. HTML/CSS: Creating well-structured and visually appealing front-end components.
+									<br>
+									6. JavaScript: Implementing client-side logic and enhancing user interactivity.
+									<br>
+									7. React: Building dynamic and responsive user interfaces for modern web applications.</li>
+								</ul></p>
+								
+
+							<!-- skills progress -->
+						
+							<div class="clearfix"></div>
+
+							<a href="" download>
+								<span class="buton buton-bg">Download CV</span>
+							</a>
+							<a href="#0" data-scroll-nav="6">
+								<span class="buton">Contact Me</span>
+							</a>
+
+						</div>
+					</div>
+				</div><!-- /row -->
+			</div><!-- /container -->
+		</section>
+		<!-- ====== End Hero ======  -->
+
+		<section class="portfolio section-padding" style="padding: 20px 30px;"  data-scroll-index="2">
+			
+
+					<!-- section heading -->
+					<div class="section-head">
+						<h3>My Tech Stack.</h3>
+					</div>
+
+					<!-- filter links -->
+					<!-- <div class="filtering text-center mb-50">
+						<span data-filter='*' class="active">All</span>
+						<span data-filter='.brand'>Brand</span>
+						<span data-filter='.web'>Design</span>
+						<span data-filter='.graphic'>Graphic</span>
+					</div> -->
+
+					<!-- gallery -->
+				
+
+						<div class="swiper mySwiper" style="height: 200px;">
+							<div class="swiper-wrapper">
+							  <div class="swiper-slide"><img src="img/portfolio/laravel.svg"  alt="" srcset=""></div>
+							  <div class="swiper-slide"><img src="img/portfolio/php.svg"  alt="" srcset=""></div>
+							  <div class="swiper-slide"><img src="img/portfolio/python.svg"  alt="" srcset=""></div>
+							  <div class="swiper-slide"><img src="img/portfolio/django.svg"  alt="" srcset=""></div>
+							  <div class="swiper-slide"><img src="img/portfolio/mysql.svg"  alt="" srcset=""></div>
+							  <div class="swiper-slide"><img src="img/portfolio/jquery.svg"  alt="" srcset=""></div>
+							  <div class="swiper-slide"><img src="img/portfolio/8666390_js_square_icon.svg"  alt="" srcset=""></div>
+							  <div class="swiper-slide"><img src="img/portfolio/html.svg"  alt="" srcset=""></div>
+							  <div class="swiper-slide"><img src="img/portfolio/7422531_css3_css_file_development_icon.svg"  alt="" srcset=""></div>
+							</div>
+							<!-- s -->
+						  </div>
+
+					
+					
+				
+		</section>
+	
+
+		<!--====== Portfolio ======-->
+		<section class="portfolio section-padding pb-70" data-scroll-index="3">
+			<div class="container">
+				<div class="row">
+
+					<!-- section heading -->
+					<div class="section-head">
+						<h3>Portfolio.</h3>
+					</div>
+
+					<!-- filter links -->
+					<div class="filtering text-center mb-50">
+						<span data-filter='*' class="active">All</span>
+						<span data-filter='.brand'>Brand</span>
+						<span data-filter='.web'>Design</span>
+						<span data-filter='.graphic'>Graphic</span>
+					</div>
+
+					<!-- gallery -->
+					<div class="gallery text-center">
+
+						<!-- gallery item -->
+						<div class="col-md-4 col-sm-6 items graphic">
+							<div class="item-img">
+								<img src="img/portfolio/1.jpg" alt="image">
+								<div class="item-img-overlay">
+									<div class="overlay-info v-middle text-center">
+										<h6 class="sm-titl">WEB DESIGN</h6>
+										<div class="icons">
+											<span class="icon">
+												<a href="#0">
+													<i class="fa fa-chain-broken" aria-hidden="true"></i>
+												</a>
+											</span>
+
+											<span class="icon link">
+												<a href="img/portfolio/1.jpg">
+													<i class="fa fa-search-plus" aria-hidden="true"></i>
+												</a>
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!-- gallery item -->
+						<div class="col-md-4 col-sm-6 items web">
+							<div class="item-img">
+								<img src="img/portfolio/2.jpg" alt="image">
+								<div class="item-img-overlay">
+									<div class="overlay-info v-middle text-center">
+										<h6 class="sm-titl">WEB DESIGN</h6>
+										<div class="icons">
+											<span class="icon">
+												<a href="#0">
+													<i class="fa fa-chain-broken" aria-hidden="true"></i>
+												</a>
+											</span>
+
+											<span class="icon link">
+												<a href="img/portfolio/2.jpg">
+													<i class="fa fa-search-plus" aria-hidden="true"></i>
+												</a>
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!-- gallery item -->
+						<div class="col-md-4 col-sm-6 items graphic">
+							<div class="item-img">
+								<img src="img/portfolio/3.jpg" alt="image">
+								<div class="item-img-overlay">
+									<div class="overlay-info v-middle text-center">
+										<h6 class="sm-titl">WEB DESIGN</h6>
+										<div class="icons">
+											<span class="icon">
+												<a href="#0">
+													<i class="fa fa-chain-broken" aria-hidden="true"></i>
+												</a>
+											</span>
+
+											<span class="icon link">
+												<a href="img/portfolio/3.jpg">
+													<i class="fa fa-search-plus" aria-hidden="true"></i>
+												</a>
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!-- gallery item -->
+						<div class="col-md-4 col-sm-6 items brand">
+							<div class="item-img">
+								<img src="img/portfolio/4.jpg" alt="image">
+								<div class="item-img-overlay">
+									<div class="overlay-info v-middle text-center">
+										<h6 class="sm-titl">WEB DESIGN</h6>
+										<div class="icons">
+											<span class="icon">
+												<a href="#0">
+													<i class="fa fa-chain-broken" aria-hidden="true"></i>
+												</a>
+											</span>
+
+											<span class="icon link">
+												<a href="img/portfolio/4.jpg">
+													<i class="fa fa-search-plus" aria-hidden="true"></i>
+												</a>
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!-- gallery item -->
+						<div class="col-md-4 col-sm-6 items web graphic">
+							<div class="item-img">
+								<img src="img/portfolio/5.jpg" alt="image">
+								<div class="item-img-overlay">
+									<div class="overlay-info v-middle text-center">
+										<h6 class="sm-titl">WEB DESIGN</h6>
+										<div class="icons">
+											<span class="icon">
+												<a href="#0">
+													<i class="fa fa-chain-broken" aria-hidden="true"></i>
+												</a>
+											</span>
+
+											<span class="icon link">
+												<a href="img/portfolio/5.jpg">
+													<i class="fa fa-search-plus" aria-hidden="true"></i>
+												</a>
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!-- gallery item -->
+						<div class="col-md-4 col-sm-6 items brand">
+							<div class="item-img">
+								<img src="img/portfolio/6.jpg" alt="image">
+								<div class="item-img-overlay">
+									<div class="overlay-info v-middle text-center">
+										<h6 class="sm-titl">WEB DESIGN</h6>
+										<div class="icons">
+											<span class="icon">
+												<a href="#0">
+													<i class="fa fa-chain-broken" aria-hidden="true"></i>
+												</a>
+											</span>
+
+											<span class="icon link">
+												<a href="img/portfolio/6.jpg">
+													<i class="fa fa-search-plus" aria-hidden="true"></i>
+												</a>
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div><!-- /row -->
+			</div><!-- /container -->
+		</section>
+		<!--====== End Portfolio ======-->
+
+
+
+		<!--====== Contact ======-->
+		<section class="contact section-padding" data-scroll-index="6">
+			<div class="container">
+				<div class="row ">
+					
+					<!-- section heading -->
+					<div class="section-head">
+						<h3>Contact Us.</h3>
+					</div>
+					<div class="col-md-offset-1  col-md-10">
+
+						<!-- contact info -->
+						<div class="info text-center  mb-50">
+
+							<div class="col-md-6">
+								<div class="item">
+									<span class="icon"><i class="fa fa-location-arrow" aria-hidden="true"></i></span>
+									<h6>Address</h6>
+									<p>Thane, Maharashtra</p>
+								</div>
+							</div>
+
+							<div class="col-md-4">
+								<a href="mailto:lalitharwate90@gmail.com">
+								<div class="item">
+									<span class="icon"><i class="fa fa-envelope" aria-hidden="true"></i></span>
+									<h6>Email</h6>
+									<p>lalitharwate90@gmail.com</p>
+								</div>
+								</a>	
+							</div>
+
+						
+							<div class="clearfix"></div>
+						</div>
+
+						<!-- contact form -->
+						<form class="form" id="contact-form" method="post" action="contact.php">
+		                    <div class="messages"></div>
+
+		                    <div class="controls">
+
+		                        <div class="row">
+		                            <div class="col-md-6">
+		                                <div class="form-group">
+		                                    <input id="form_name" type="text" name="name" placeholder="Name" required="required">
+		                                </div>
+		                            </div>
+
+		                             <div class="col-md-6">
+		                                <div class="form-group">
+		                                    <input id="form_email" type="email" name="email" placeholder="Email" required="required">
+		                                </div>
+		                            </div>
+		                        </div>
+		                        <div class="row">
+		                            <div class="col-md-12">
+		                                <div class="form-group">
+		                                    <textarea id="form_message" name="message" placeholder="Message" rows="4" required="required"></textarea>
+		                                </div>
+
+		                                <input type="submit" value="Submit" class="buton buton-bg">
+		                            </div>
+		                        </div>
+		                    </div>
+		                </form>
+
+					</div>
+				</div><!-- /row -->
+			</div><!-- /container -->
+		</section>
+		<!--====== End Contact ======-->
+
+		<!--====== Footer ======-->
+		<footer>
+			<div class="container text-center">
+				<div class="row">
+					<p>CopyRight 2024 &copy; By Lalit All Rights Reserved</p>
+				</div>
+			</div>
+		</footer>
+		<!--====== End Footer ======-->
+
+
+
+       
+        <!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+		<script src="https://code.jquery.com/jquery-migrate-3.0.0.min.js"></script>
+
+	  	<!-- bootstrap -->
+		<script src="js/bootstrap.min.js"></script>
+
+		<!-- scrollIt -->
+		<script src="js/scrollIt.min.js"></script>
+
+		<!-- magnific-popup -->
+		<script src="js/jquery.magnific-popup.min.js"></script>
+
+		<!-- owl carousel -->
+		<script src="js/owl.carousel.min.js"></script>
+
+		<!-- stellar js -->
+		<script src="js/jquery.stellar.min.js"></script>
+
+		<!-- animated.headline -->
+		<script src="js/animated.headline.js"></script>
+
+      	<!-- jquery.waypoints.min js -->
+	  	<script src="js/jquery.waypoints.min.js"></script>
+
+	  	<!-- jquery.counterup.min js -->
+	  	<script src="js/jquery.counterup.min.js"></script>
+
+      	<!-- isotope.pkgd.min js -->
+      	<script src="js/isotope.pkgd.min.js"></script>
+
+      	<!-- validator js -->
+      	<script src="js/validator.js"></script>
+
+      	<!-- custom script -->
+        <script src="js/custom.js"></script>
+
+		<script>
+			var swiper = new Swiper(".mySwiper", {
+			  spaceBetween: 0,
+			  centeredSlides: false,
+			  autoplay: {
+				delay: 1000,
+				disableOnInteraction: false,
+			  },
+			  pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			  },
+			  navigation: {
+				nextEl: ".swiper-button-next",
+				prevEl: ".swiper-button-prev",
+			  },
+
+			  breakpoints: {
+				200:{
+					slidesPerView: 3,
+          spaceBetween: 20,
+				},
+        640: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      },
+			});
+		  </script>
+
+    </body>
+</html>
